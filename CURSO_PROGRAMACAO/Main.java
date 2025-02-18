1b8faf2dfa78c2f80c89ae79a239cada3f0c6a04
Luanna Silva <luannaevellyn2020@gmail.com>
2025-02-18 13:37:55 +0000

    COMIT

---
+++ CURSO_PROGRAMACAO/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        double x = 10.35784;
+        System.out.println(y);
+        System.out.println("");
+
+
+
+
+
+    }
+
+
+}
