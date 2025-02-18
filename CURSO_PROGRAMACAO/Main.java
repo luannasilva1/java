4709e28c053add91d9035bc13d65717c078a7870
Luanna Silva <luannaevellyn2020@gmail.com>
2025-02-18 13:45:50 +0000

    formatação

--- CURSO_PROGRAMACAO/Main.java
+++ CURSO_PROGRAMACAO/Main.java
@@ -1,11 +1,14 @@
+import java.util.Locale;
+
+
 public class Main {
     public static void main(String[] args) {
         
         double x = 10.35784;
-        System.out.println(y);
-        System.out.println("");
-
-
+        System.out.println(x);
+        System.out.printf("%.2f%n", x);
+        Locale.setDefault(Locale.US);
+        System.out.println("RESULTADO = " + x + " METROS");
 
 
 
